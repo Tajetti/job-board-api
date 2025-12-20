c683e9d5fbed30ad3b9a081c0f78a39dff444367
Alvaro Tajetti <talhettialvaro@gmail.com>
2025-12-20 12:26:28 -0300

    Inserindo validação

--- src/main/java/br/com/alvaro/GestaoVagas/Modules/Candidate/Controllers/CandidateController.java
+++ src/main/java/br/com/alvaro/GestaoVagas/Modules/Candidate/Controllers/CandidateController.java
@@ -1,6 +1,7 @@
 package br.com.alvaro.GestaoVagas.Modules.Candidate.Controllers;
 
 import br.com.alvaro.GestaoVagas.Modules.Candidate.Entity.CandidateEntity;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CandidateController {
 
     @PostMapping("/create")
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidato: " + candidateEntity.getEmail());
     }
 }
--- src/main/java/br/com/alvaro/GestaoVagas/Modules/Candidate/Entity/CandidateEntity.java
+++ src/main/java/br/com/alvaro/GestaoVagas/Modules/Candidate/Entity/CandidateEntity.java
@@ -1,6 +1,9 @@
 package br.com.alvaro.GestaoVagas.Modules.Candidate.Entity;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -9,8 +12,13 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+    @Pattern(regexp = "ˆ(?!\\s*$).+", message = "username não pode conter espaço")
     private String username;
+
+    @Email(message = "O campo deve conter um email válido!")
     private String email;
+
+    @Length(min = 10, max = 100, message = "Sua senha deve ter entre 10 e 100 caracteres")
     private String password;
     private String description;
     private String curriculum;
