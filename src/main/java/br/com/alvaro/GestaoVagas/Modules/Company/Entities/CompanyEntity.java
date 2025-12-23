937a0370fed91d19ab62b474decaa245ba9e5b8a
Alvaro Tajetti <talhettialvaro@gmail.com>
2025-12-23 15:14:26 -0300

    criando a company entity

--- src/main/java/br/com/alvaro/GestaoVagas/Modules/Company/Entities/CompanyEntity.java
+++ src/main/java/br/com/alvaro/GestaoVagas/Modules/Company/Entities/CompanyEntity.java
@@ -1,4 +1,4 @@
-package br.com.alvaro.GestaoVagas.Modules.Candidate.Entity;
+package br.com.alvaro.GestaoVagas.Modules.Company.Entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,33 +7,34 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
-
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.data.annotation.CreatedBy;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Entity(name = "company")
 @Data
-@Entity(name = "candidate")
-public class CandidateEntity {
+public class CompanyEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    private String name;
 
     @Pattern(regexp = "^[^\s]+$", message = "O campo [username] não deve conter espaço")
     private String username;
 
-    @Email(message = "O campo [email] deve conter um email válido!")
+    @Email(message = "Insira um email válido")
     private String email;
 
-    @Length(min = 10, max = 100, message = "Sua senha deve ter entre 10 e 100 caracteres")
+    @Length(min = 10, max = 100, message = "A senha deve ter de 10 a 100 caracteres")
     private String password;
+    private String website;
+    private String name;
     private String description;
-    private String curriculum;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
 }
