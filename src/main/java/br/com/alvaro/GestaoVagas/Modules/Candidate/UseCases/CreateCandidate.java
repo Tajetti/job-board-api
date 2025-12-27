3549da3f09f2e6495b227390c8dba71786e7fbee
Alvaro Tajetti <talhettialvaro@gmail.com>
2025-12-27 18:44:31 -0300

    criando hash do candidato

--- src/main/java/br/com/alvaro/GestaoVagas/Modules/Candidate/UseCases/CreateCandidate.java
+++ src/main/java/br/com/alvaro/GestaoVagas/Modules/Candidate/UseCases/CreateCandidate.java
@@ -4,6 +4,7 @@ import br.com.alvaro.GestaoVagas.Exception.UserFoundedException;
 import br.com.alvaro.GestaoVagas.Modules.Candidate.Entity.CandidateEntity;
 import br.com.alvaro.GestaoVagas.Modules.Candidate.Repository.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +13,18 @@ public class CreateCandidate {
     @Autowired
     private CandidateRepository repository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidate) {
         this.repository
                 .findByUsernameOrEmail(candidate.getUsername(), candidate.getEmail())
                 .ifPresent((cand) -> {
                     throw new UserFoundedException("Usuário já existe!");
                 });
+
+                var password = passwordEncoder.encode(candidate.getPassword());
+                candidate.setPassword(password);
 
         return this.repository.save(candidate);
     }
